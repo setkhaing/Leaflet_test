ef1ad086fed1dbf1e50ca2df00223941fe939f2f
setkhaing <32797915+setkhaing@users.noreply.github.com>
2018-03-14 23:28:37 +0630

    Add files via upload

---
+++ Damlocation0.js
@@ -0,0 +1,11 @@
+var json_Damlocation0={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "qc_id": 0.000000, "oid_": 0.000000, "name": "Gaw Lang Dam", "folderpath": "Tanks_final_GoogleEarth.kmz\/My Drawings", "symbolid": 0.000000, "altmode": -1.000000, "base": 0.000000, "snippet": null, "popupinfo": null, "haslabel": -1.000000, "labelid": 0.000000 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 98.580365615000062, 26.066991465000058, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 1.000000, "oid_": 0.000000, "name": "Khan Kang Dam", "folderpath": "Tanks_final_GoogleEarth.kmz\/My Drawings", "symbolid": 0.000000, "altmode": -1.000000, "base": 0.000000, "snippet": null, "popupinfo": null, "haslabel": -1.000000, "labelid": 0.000000 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 98.431573280000066, 26.024981900000057, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 2.000000, "oid_": 0.000000, "name": "Toneshinchoung Dam", "folderpath": "Tanks_final_GoogleEarth.kmz\/My Drawings", "symbolid": 0.000000, "altmode": -1.000000, "base": 0.000000, "snippet": null, "popupinfo": null, "haslabel": -1.000000, "labelid": 0.000000 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 98.381367050000051, 25.966311271000052, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 3.000000, "oid_": 0.000000, "name": "Law din Dam", "folderpath": "Tanks_final_GoogleEarth.kmz\/My Drawings", "symbolid": 0.000000, "altmode": -1.000000, "base": 0.000000, "snippet": null, "popupinfo": null, "haslabel": -1.000000, "labelid": 0.000000 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 98.319227272000035, 26.10098782700004, 0.0 ] ] } }
+]
+}
